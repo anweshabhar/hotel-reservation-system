823075361cf609ef3097550d71f733d351b07d60
Anwesha <anwesha.bhar@gmail.com>
2021-01-17 18:28:19 +0530

    Updated swagger dependency

--- hotel-information-service/src/main/java/com/example/hotelinformationservice/HotelInformationServiceApplication.java
+++ hotel-information-service/src/main/java/com/example/hotelinformationservice/HotelInformationServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,7 +24,8 @@ public class HotelInformationServiceApplication {
 	public Docket swaggerConfiguration() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.paths(PathSelectors.ant("/hotel/*"))
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
 				.build();
 	}
 
